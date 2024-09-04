2523ed7c1638b08bd6324ec864e842756a1b3efb
dr250385 <dr250385@WIN-10M20X2>
2024-09-04 13:25:17 +0200

    Introduced Login and Signup form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,41 @@
+import { useState } from 'react';
+
 // styles
 import styles from './Login.module.scss';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(`Email: ${email} Password: ${password}`);
+  };
+
+  return (
+    <form className={styles['login-form']} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+
+      <button className='btn'>Login</button>
+    </form>
+  );
 }
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,54 @@
 // styles
+import { useState } from 'react';
+
+// styles
 import styles from './Signup.module.scss';
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(
+      `Email: ${email} Password: ${password} Display Name: ${displayName}`
+    );
+  };
+
+  return (
+    <form className={styles['signup-form']} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+
+      <label>
+        <span>display name:</span>
+        <input
+          type='text'
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+
+      <button className='btn'>Login</button>
+    </form>
+  );
 }
