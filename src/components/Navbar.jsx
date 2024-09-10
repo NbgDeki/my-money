7299b1be9fc150189ffd5358623691ec6c015513
dr250385 <dr250385@WIN-10M20X2>
2024-09-10 14:48:27 +0200

    conditionally showing user content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // styles
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
@@ -14,19 +16,26 @@ export default function Navbar() {
           <Link to='/'>myMoney</Link>
         </li>
 
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
 
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-
-        <li>
-          <button className='btn' onClick={logout}>
-            Logout
-          </button>
-        </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className='btn' onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
