69a0e108e1e76fca834c54e73a311f2ef7ed7d26
dr250385 <dr250385@WIN-10M20X2>
2024-09-11 11:04:49 +0200

    waiting until auth is ready and show user logged in

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
   Route
 } from 'react-router-dom/cjs/react-router-dom.min';
 
+import { useAuthContext } from './hooks/useAuthContext';
+
 // components
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
@@ -11,24 +13,28 @@ import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Switch>
-          <Route exact path='/'>
-            <Home></Home>
-          </Route>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Switch>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
 
-          <Route path='/login'>
-            <Login></Login>
-          </Route>
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
 
-          <Route path='/signup'>
-            <Signup></Signup>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+            <Route path='/signup'>
+              <Signup></Signup>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
