19b2139bd4714ffbcfdb738d0a22a657feed54bc
dr250385 <dr250385@WIN-10M20X2>
2024-09-11 11:56:58 +0200

    crating a useFirestore hook

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,23 @@
+import { useReducer, useEffect, useState } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: null
+};
+
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    case value:
+
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState);
+  const [isCancelled, setIsCancelled] = useState(false);
+};
