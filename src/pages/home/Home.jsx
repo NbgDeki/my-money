5a163570fd4d283a7e6949a1e98b0f409f626186
dr250385 <dr250385@WIN-10M20X2>
2024-09-11 11:39:53 +0200

    adding transaction form to the homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,16 @@
 // styles
 import styles from './Home.module.scss';
 
+// components
+import TransactionForm from './TransactionForm';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm></TransactionForm>
+      </div>
+    </div>
+  );
 }
